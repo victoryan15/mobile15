42bd099eace1b71e2765691f186026f8823a6312
Victor Yan De Oliveira Ferreira <133805971+victoryan15@users.noreply.github.com>
2023-07-24 17:55:53 -0300

    Add files via upload

---
+++ calc_js/index2.js/index3.js
@@ -0,0 +1,23 @@
+function clicou(){
+    var media=document.getElementById('media')
+    var br1=document.getElementById('barra1')
+    var br2=document.getElementById('barra2')
+    var br1=Number(br1.value)
+    var br2=Number(br2.value)
+    var media=String(media.String)
+   var valor=[600-(b1*2+b2*3)]/5
+   document.getElementById('media').innerHTML=valor
+   document.getElementById('nota-necessaria').innerHTML='nota necessaria'
+}
+
+
+
+
+
+
+
+
+
+
+
+
