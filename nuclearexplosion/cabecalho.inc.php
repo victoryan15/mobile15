98c3dbbc0549a29509c5fcd9838ef38f6c54c2c3
Victor Yan De Oliveira Ferreira <133805971+victoryan15@users.noreply.github.com>
2023-08-01 16:32:51 -0300

    Add files via upload

---
+++ nuclearexplosion/cabecalho.inc.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NuclearExplosion</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+    <header>
+    <div class="topo">
+    <h1>esse é o site da banda NuclearExplosion</h1>
+    <img src="img/logo.png" >
+    <div class="menu_opcoes">
+        <ul>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Integrantes</a></li>
+        <li><a href="#">Shows</a></li>
+        <li><a href="#">Noticias</a></li>
+        <li><a href="#">Musicas</a></li>
+        <li><a href="#">Loja</a></li>
+        <li><a href="#">Contato comercial</a></li>
+    <ul>
+</div>
+</body>
+</header>
+</html>
