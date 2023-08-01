8b3467207c0af909b38793f80891b7a995d20293
Victor Yan De Oliveira Ferreira <133805971+victoryan15@users.noreply.github.com>
2023-08-01 16:27:12 -0300

    Add files via upload

---
+++ atividadecarlos2g/atividade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="topo">
+        <div class="logo"></div>
+        <div class="menu"></div>
+    </div>
+    <div class="destaque"></div>
+    <div class="corpo">
+        <div class="painel1"></div>
+        <div class="painel2"></div>
+        <div class="painel3"></div>
+        <div class="painel4"></div>
+    </div>
+    <div class="rodape"></div>
+</body>
+</html>
